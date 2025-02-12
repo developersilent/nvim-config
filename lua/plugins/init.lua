23a4ec3586d620874d75e3ab9a7fd47227341e48
= <=>
2025-02-12 14:17:28 +0000

    first commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "vyfor/cord.nvim",
+    build = ":Cord update",
+    lazy = false,
+    config = function()
+      require "configs.cord"
+    end,
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "LspAttach", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require "configs.error"
+      vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end,
+  },
+}
